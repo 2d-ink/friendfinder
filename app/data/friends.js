49f8c0205e4eec620dcef1b000a70dd6a0ec20de
Ryan Nguyen <ryan@Ryans-MBP.hsd1.ut.comcast.net>
2019-02-10 21:06:36 -0700

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends =[{
+
+    name: "Tom Brady", 
+    photo: "https://d28hgpri8am2if.cloudfront.net/author_images/3604899/tom-brady-2141862582.jpg",
+    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 4] 
+
+},{
+    name: "Brad Pit", 
+    photo: "https://data1.ibtimes.co.in/cache-img-0-450/en/full/640844/1549689660_brad-pitt.jpg",
+    scores:[3, 4, 5, 1, 4, 3, 3, 4, 5, 2]
+},{
+    name: "Megan Fox", 
+    photo: "https://celebshistory.com/wp-content/uploads/2018/08/Megan-Fox-before-and-after-plastic-surgery.jpg",
+    scores:[4, 4, 4, 5, 5, 5, 3, 3, 2, 1]
+},{
+    name: "Jasmine Tookes",
+    photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2018424/rs_1080x1350-180524123933-1080.Jasmine-Tookes-Perfect-Selfie.jl.052418.jpg?fit=inside|900:auto&output-quality=90",
+    scores:[2, 2, 2, 2, 2, 2, 2, 2, 2, 2] 
+},{
+    name: "Bill Clinton", 
+    photo: "http://specials-images.forbesimg.com/imageserve/5638e0f8e4b0ffa7afe6a0ab/280x425.jpg?fit=scale&background=000000",
+    scores:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}];
