0dc9ea5b9e75adb6823a4c2afd2554cec0a9277e
Ryan Nguyen <ryan@Ryans-MBP.hsd1.ut.comcast.net>
2019-02-11 23:47:11 -0700

    new upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,77 @@
-var friends =[{
+var friends = [
+  {
+    name: "Tom Hardy",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Jamie Dornan",
+    photo:
+      "https://www.usmagazine.com/wp-content/uploads/2018/02/jamie-dornan-nude.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    name: "Anthony Joshua",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    name: "Henry Cavill",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTUxNTExMzUzOF5BMl5BanBnXkFtZTgwOTI1MjA3OTE@._V1_.jpg",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    name: "Emily Ratajowski",
+    photo:
+      "https://pmchollywoodlife.files.wordpress.com/2014/10/Emily-Ratajkowski-Reveals-Rock-Hard-Abs-In-Teeny-Crop-Top-While-Vacationing-With-Husband-gallery.jpg",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    name: "Blake Lively",
+    photo:
+      "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    name: "Kate Upton",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  },
+  {
 
     name: "Tom Brady", 
     photo: "https://d28hgpri8am2if.cloudfront.net/author_images/3604899/tom-brady-2141862582.jpg",
     scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 4] 
 
-},{
+},
+{
     name: "Brad Pit", 
     photo: "https://data1.ibtimes.co.in/cache-img-0-450/en/full/640844/1549689660_brad-pitt.jpg",
     scores:[3, 4, 5, 1, 4, 3, 3, 4, 5, 2]
-},{
+},
+{
     name: "Megan Fox", 
     photo: "https://celebshistory.com/wp-content/uploads/2018/08/Megan-Fox-before-and-after-plastic-surgery.jpg",
     scores:[4, 4, 4, 5, 5, 5, 3, 3, 2, 1]
-},{
+},
+{
     name: "Jasmine Tookes",
     photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2018424/rs_1080x1350-180524123933-1080.Jasmine-Tookes-Perfect-Selfie.jl.052418.jpg?fit=inside|900:auto&output-quality=90",
     scores:[2, 2, 2, 2, 2, 2, 2, 2, 2, 2] 
-},{
+},
+{
     name: "Bill Clinton", 
     photo: "http://specials-images.forbesimg.com/imageserve/5638e0f8e4b0ffa7afe6a0ab/280x425.jpg?fit=scale&background=000000",
     scores:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-},{
+},
+{
     name: "Blake Lively ", 
     photo: "https://24smi.org/public/media/235x307/celebrity/2018/03/13/w2lyetrs6b4e-blake-lively.jpg",
     scores:[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-}];
+}
+];
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-const port = process.env.PORT || 3000;
-
-const app = express();
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('./app/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require('./app/routing/htmlRoutes')(app)
-require('./app/routing/apiRouting')(app)
+app.use(express.static("app/public"));
 
-app.listen(port, function () {
-  `listening on ${port}, have fun!`
-})
+require("./app/routing/api-routes")(app);
+require("./app/routing/html-routes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
